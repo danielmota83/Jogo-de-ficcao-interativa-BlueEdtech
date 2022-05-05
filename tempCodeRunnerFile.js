f8ad6455d41157964f2f7666776f5d61134dada1
Daniel Mota <101141570+danielmota83@users.noreply.github.com>
2022-05-04 22:46:31 -0300

    arquivo js deletado

--- tempCodeRunnerFile.js
+++
@@ -1,6 +0,0 @@
-
-        console.log();
-        resposta = prompt('').toLowerCase();
-        if (resposta !== 'sim' && resposta !== 'nao') {
-            console.log(
-                '\nVocê está emocionado com esse jogo elet
